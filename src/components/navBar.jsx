d44e611c4608552bd397079ab93290aeb853acb3
SimonGideon <simongideon918@gmail.com>
2024-07-14 22:02:55 +0300

    Initial commit

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -2,24 +2,21 @@ const NavBar = () => {
   return (
     <header className="flex items-center justify-between whitespace-nowrap border-b border-solid border-b-[#f0f2f4] px-10 py-3">
       <div className="flex items-center gap-4 text-[#111318]">
-        <div className="size-4">
-          <svg
-            viewBox="0 0 48 48"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M4 4H17.3334V17.3334H30.6666V30.6666H44V44H4V4Z"
-              fill="currentColor"
-            ></path>
-          </svg>
+        <div className="size-6">
+          <img
+            width="90"
+            height="90"
+            className="w-8"
+            src="https://img.icons8.com/ios-glyphs/90/book.png"
+            alt="book"
+          />
         </div>
         <h2 className="text-[#111318] text-lg font-bold leading-tight tracking-[-0.015em]">
-          Bookshelf
+          FaithReads
         </h2>
       </div>
       <div className="flex flex-1 justify-end gap-8">
-        <label className="flex flex-col min-w-40 !h-10 max-w-64">
+        <label className="hidden md:flex flex-col min-w-40 !h-10 max-w-64 ">
           <div className="flex w-full flex-1 items-stretch rounded-xl h-full">
             <div
               className="text-[#636e88] flex border-none bg-[#f0f2f4] items-center justify-center pl-4 rounded-l-xl border-r-0"
@@ -45,27 +42,13 @@ const NavBar = () => {
           </div>
         </label>
         <div className="flex gap-2">
-          <button className="flex min-w-[84px] max-w-[480px] cursor-pointer items-center justify-center overflow-hidden rounded-xl h-10 px-4 bg-[#f0f2f4] text-[#111318] text-sm font-bold leading-normal tracking-[0.015em]">
-            <span className="truncate">All Books</span>
-          </button>
-          <button className="flex max-w-[480px] cursor-pointer items-center justify-center overflow-hidden rounded-xl h-10 bg-[#f0f2f4] text-[#111318] gap-2 text-sm font-bold leading-normal tracking-[0.015em] min-w-0 px-2.5">
-            <div
-              className="text-[#111318]"
-              data-icon="Bookmark"
-              data-size="20px"
-              data-weight="regular"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="20px"
-                height="20px"
-                fill="currentColor"
-                viewBox="0 0 256 256"
-              >
-                <path d="M184,32H72A16,16,0,0,0,56,48V224a8,8,0,0,0,12.24,6.78L128,193.43l59.77,37.35A8,8,0,0,0,200,224V48A16,16,0,0,0,184,32Zm0,16V161.57l-51.77-32.35a8,8,0,0,0-8.48,0L72,161.56V48ZM132.23,177.22a8,8,0,0,0-8.48,0L72,209.57V180.43l56-35,56,35v29.14Z"></path>
-              </svg>
-            </div>
-          </button>
+          <img
+            width="50"
+            height="50"
+            className="size-8"
+            src="https://img.icons8.com/material-outlined/50/gender-neutral-user.png"
+            alt="gender-neutral-user"
+          />
         </div>
       </div>
     </header>
