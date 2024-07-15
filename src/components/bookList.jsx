f1a249358919672da97cd0574e5b48690fbd04d2
SimonGideon <simongideon918@gmail.com>
2024-07-16 02:14:58 +0300

    fix cursor effect on See All

--- src/components/bookList.jsx
+++ src/components/bookList.jsx
@@ -5,7 +5,7 @@ const BooksList = ({ books }) => {
     <section className="shadow-lg shadow-bunker-300 px-6 my-10 rounded-xl ">
       <div className="flex justify-between items-baseline">
         <h2 className="py-6 font-semibold text-lg">Trending Now</h2>
-        <p className="font-medium underline">See All</p>
+        <p className="font-medium underline cursor-pointer">See All</p>
       </div>
 
       <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-col-5 xl:grid-col-6 justify-items-center gap-4">
