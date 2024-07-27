c6ba6eea8aba0866f57a202d99a44dc959aaa505
SimonGideon <simongideon918@gmail.com>
2024-07-27 22:05:51 +0300

    fix number of book loads per page

--- src/components/bookList.jsx
+++ src/components/bookList.jsx
@@ -1,6 +1,7 @@
 import PropTypes from "prop-types";
 
 const BooksList = ({ books }) => {
+  const chosen = books.slice(0, 12);
   return (
     <section className="shadow-lg shadow-bunker-300 px-6 my-10 rounded-xl ">
       <div className="flex justify-between items-baseline">
@@ -9,8 +10,8 @@ const BooksList = ({ books }) => {
       </div>
       <hr />
       <div className="grid pt-5 grid-cols-2 md:grid-cols-4 lg:grid-col-5 xl:grid-col-6 justify-items-center gap-4">
-        {books &&
-          books.map((book) => (
+        {chosen &&
+          chosen.map((book) => (
             <div key={book.id} className="">
               <img
                 src={book.cover_image_url}
