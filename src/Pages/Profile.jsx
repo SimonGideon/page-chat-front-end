bda5cfc2f5a7a2acbed39c2094404a00e8e1ffcd
SimonGideon <simongideon918@gmail.com>
2024-08-04 23:48:19 +0300

    chore: mange current user state caching

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -1,7 +1,21 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getCurrentUser } from "@/redux/features/authSlice";
+import { Loader, NavBar } from "@/components";
 const Profile = () => {
+  const dispatch = useDispatch();
+  const { user, loading, error } = useSelector((state) => state.auth);
+  useEffect(() => {
+    // Fetch current user details when component mounts
+    dispatch(getCurrentUser());
+  }, [dispatch]);
   return (
     <>
+      <NavBar />
       <div className="container">
+        {loading && <Loader />}
+        {error && <p>Error: {error.message}</p>}
+        {user && <h2>Welcome, {user.first_name}</h2>}
         <h2>Welcome</h2>
       </div>
     </>
