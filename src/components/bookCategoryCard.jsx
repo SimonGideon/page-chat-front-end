060f3e408301cdce2a71e1dc910f01c5ee6942d4
SimonGideon <simongideon918@gmail.com>
2024-08-16 20:41:53 +0300

    Initial commit

--- src/components/bookCategoryCard.jsx
+++ src/components/bookCategoryCard.jsx
@@ -1,31 +1,31 @@
 import PropTypes from "prop-types";
-const BookCategoryCard = ({ category, books }) => {
+
+const BookCategoryCard = ({ category, books, bg }) => {
   return (
     <div className="bg-white shadow-lg rounded-lg overflow-hidden max-w-xs mx-auto">
       <div className="p-4">
         <h3 className="text-lg font-semibold text-gray-800 mb-2">
           {category.name}
         </h3>
-        <div className="grid grid-cols-2 gap-2">
-          {books.slice(0, 4).map((book) => (
-            <div key={book.id} className="relative">
+        <div className={`grid grid-cols-3 gap-1 bg-${bg} p-2`}>
+          {books.slice(0, 6).map((book) => (
+            <div
+              key={book.id}
+              className=" cartegory-pic relative h-fit border-4 border-slate-100"
+            >
               <img
                 src={book.cover_image_url}
                 alt={book.title}
-                className="w-1/2 object-cover rounded-lg"
+                className="w-full h-2/3 object-cover rounded-lg"
               />
             </div>
           ))}
         </div>
       </div>
-      <div className="bg-gray-100 text-center py-2">
-        <p className="text-sm text-gray-600">{category.name}</p>
-      </div>
     </div>
   );
 };
 
-// validate the props passed to the component
 BookCategoryCard.propTypes = {
   category: PropTypes.shape({
     id: PropTypes.string.isRequired,
@@ -39,6 +39,7 @@ BookCategoryCard.propTypes = {
       cover_image_url: PropTypes.string,
     })
   ).isRequired,
+  bg: PropTypes.string,
 };
 
 export default BookCategoryCard;
--- src/components/booksCategoryDisplay.jsx
+++ src/components/booksCategoryDisplay.jsx
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 
 const BooksCategoryDisplay = ({ data }) => {
   const categories = {};
+  const categoryColors = ["bg1_category", "bg2_category", "bg3_category"];
 
   data.forEach((book) => {
     if (!categories[book.category.id]) {
@@ -17,19 +18,20 @@ const BooksCategoryDisplay = ({ data }) => {
   const categoryArray = Object.values(categories).slice(0, 4);
 
   return (
-    <div className="p-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-      {categoryArray.map((category) => (
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+      {categoryArray.map((category, index) => (
         <BookCategoryCard
           key={category.id}
           category={category}
           books={category.books}
+          bg={categoryColors[index] || "bg-default"}
         />
       ))}
     </div>
   );
 };
 
-// validate the props passed to the component
+// Validate the props passed to the component
 BooksCategoryDisplay.propTypes = {
   data: PropTypes.arrayOf(
     PropTypes.shape({
