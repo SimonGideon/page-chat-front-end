e81648d2abf672fe8933e1c6bd3a4500b9304142
SimonGideon <simongideon918@gmail.com>
2024-11-14 23:45:38 +0300

    fix node version ans Tailwind ES syntax

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-undef */
 // /** @type {import('tailwindcss').Config} */
 import tailwindcssAnimate from "tailwindcss-animate";
-module.exports = {
+export default {
   darkMode: ["class"],
   content: [
     "./pages/**/*.{js,jsx}",
