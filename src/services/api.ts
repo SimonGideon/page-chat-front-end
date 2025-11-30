27c52c5b51427bf745ab31eb70624d75735d4cdc
Simon Gideon <simongideon918@gmail.com>
2025-11-30 11:38:24 +0300

    Update SignUp form to use GlobalDropdown component, add countries and cities dropdowns with search functionality

---
+++ src/services/api.ts
@@ -0,0 +1,12 @@
+import axiosInstance from "@/redux/utils/axiosInstance";
+
+export const apiClient = {
+  post: async <T = unknown>(url: string, data?: unknown): Promise<T> => {
+    const response = await axiosInstance.post<T>(url, data);
+    return response.data;
+  },
+  get: async <T = unknown>(url: string): Promise<T> => {
+    const response = await axiosInstance.get<T>(url);
+    return response.data;
+  },
+};
